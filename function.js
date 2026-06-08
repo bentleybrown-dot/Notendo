e66f09a8b8e29cde136b6e8418e0c308f4edae42
bentleybrown-dot <bentley.brown@cambridgeinternationalacademy.ca>
2026-06-08 17:52:46 +0000

    inital

---
+++ function.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const button = document.getElementById('actionButton');
+  button.addEventListener('click', function () {
+    alert('Function.js is running!');
+  });
+});
